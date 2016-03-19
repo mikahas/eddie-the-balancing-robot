5492b14f9c11035ca58d03fbc4acf826f1809a12
Mika Hassinen <Mika Hassinen>
2016-03-19 21:54:35 +0200

    Update: (loop) added sensor interrupt handling

--- eddie/eddie.ino
+++ eddie/eddie.ino
@@ -7,6 +7,9 @@
 #include "serial-log.h"
 
 // defines
+
+#define DEBUG_INTERVAL 1000
+
 #define MIN_MOTOR_SPEED 40
 
 // right motor encoder disabled for now
@@ -31,6 +34,14 @@ volatile bool mpuInterrupt = false;	// indicates whether MPU interrupt pin has g
 uint8_t mpuIntStatus;								// actual interrupt status byte from MPU
 bool dmpReady = false;  // set true if DMP init was successful
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+long debugTimer;
+
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 MPU6050 sensor;
 
@@ -43,22 +54,23 @@ PID pid(&input, &output, &setpoint, 70, 240, 1.9, DIRECT);
 MotorDrive motors(LEFT_MOTOR_PIN_PWM, LEFT_MOTOR_PIN_A, LEFT_MOTOR_PIN_B, RIGHT_MOTOR_PIN_PWM, RIGHT_MOTOR_PIN_A, RIGHT_MOTOR_PIN_B);
 
 SerialLog logger;
+String tab = "\t";
 
 void setup() {
 
 	// start serial logger
 	logger.setup();
-	logger.msg("SETUP", "begin setup");
+	logger.msg("SETUP", "Begin setup");
 
 	Wire.begin();
 	TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
 
 	// initialize device
-	logger.msg("SETUP", "initializing sensor");
+	logger.msg("SETUP", "Initializing sensor");
 	sensor.initialize();
 
 	// verify connection
-	logger.msg("SETUP", "testing device connections...");
+	logger.msg("SETUP", "Testing device connections...");
 	if (sensor.testConnection()) {
 		logger.msg("SETUP", "Sensor connection successful.");
 	}else{
@@ -104,10 +116,68 @@ void setup() {
 }
 
 void loop() {
+	unsigned long currentTime = millis();
 
+	// if programming failed, don't try to do anything
+  if (!dmpReady) return;
+
+  // sensor mpu interrupt happened, handle data
+  if (mpuInterrupt) {
+  	handleSensorMPUData();
+  }
+
+  // debug values here
+  if ((unsigned long)(currentTime - debugTimer) >= DEBUG_INTERVAL) {
+
+  	// output from sensor
+    //logger.log(ypr[0] * 180/M_PI + tab + ypr[1] * 180/M_PI + tab + ypr[2] * 180/M_PI);
+    logger.log((String)input);
+
+  	// save debugging time
+		debugTimer = currentTime;
+  }
 }
 
+void handleSensorMPUData () {
 
+	// reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = sensor.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = sensor.getFIFOCount();
+
+  // check for overflow
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    sensor.resetFIFO();
+    logger.warning("FIFO overflow!");
+
+  // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & 0x02) {
+
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = sensor.getFIFOCount();
+
+    // read a packet from FIFO
+    sensor.getFIFOBytes(fifoBuffer, packetSize);
+
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+    sensor.dmpGetQuaternion(&q, fifoBuffer);
+    sensor.dmpGetGravity(&gravity, &q);
+    sensor.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+    // update input for PID
+    input = ypr[1] * 180/M_PI;
+ }
+}
+
+/**
+ * interrupt handler for sensor when dmp data is ready
+ */
 void dmpDataReady() {
 	mpuInterrupt = true;
 }
